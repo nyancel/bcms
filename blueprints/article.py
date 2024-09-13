37d73299cea2dd4968d5db784b7dbf18887b9ab6
human <70441087+nyancel@users.noreply.github.com>
2024-09-13 12:32:31 +0200

    Create article.py

---
+++ blueprints/article.py
@@ -0,0 +1,21 @@
+import flask
+
+bp = flask.Blueprint()
+
+@bp.post("/article/post")
+def post_article():
+
+
+@bp.post("/article/delete")
+def delete_article():
+
+@bp.post("/article/edit")
+def edit_article():
+
+@bp.post("/article/list_all")
+def list_all_article():
+
+@bp.post("/article/retrieve")
+def retrieve_article():
+
+    
