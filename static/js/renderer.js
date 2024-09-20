2cfed65c9b01ffbe3255d3e55b60f479fa0f25e8
human <70441087+nyancel@users.noreply.github.com>
2024-09-20 19:58:23 +0200

    Update renderer.js

---
+++ static/js/renderer.js
@@ -0,0 +1,51 @@
+example_article = {
+  id: "124jasdkjhasdkjhasd1-1235151987123",
+  title: "example title",
+  user_id: "124jasdkjhasdkjhasd1-1235151987123",
+  timestamp: "1726855135", // in unix seconds
+  update_timestamp: "1726855135", // in unix seconds
+  isAccepted: "true",
+  isListed: "true",
+  isDeleted: "false",
+  body: [
+    {
+      type: "paragraph",
+      text: "this is a paragraph",
+      order: 1,
+    },
+    {
+      type: "image",
+      src_id: "12345asdlkj12-1141249712365612371",
+      text: "this is an image",
+      order: 2,
+    },
+    {
+      type: "paragraph",
+      text: "this is a paragraph",
+      order: 3,
+    },
+    {
+      type: "paragraph",
+      text: "this is a paragraph",
+      order: 4,
+    },
+    {
+      type: "heading",
+      text: "this is a paragraph",
+      order: 5,
+    },
+    {
+      type: "image",
+      src_id: "12345asdlkj12-1141249712365612371",
+      text: "this is an image",
+      order: 6,
+    },
+    {
+      type: "paragraph",
+      text: "this is a paragraph",
+      order: 7,
+    },
+  ],
+};
+
+function render_article(article) {}
