dca22e5e1437f2b5418a867b424cb7c5e6030a16
human <70441087+nyancel@users.noreply.github.com>
2024-09-23 17:04:58 +0200

    editor function renames

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,19 +1,19 @@
-const article = [];
+const editor_article = [];
 
 // default objects
-const base_paragraph = {
+const editor_base_paragraph = {
   type: "paragraph",
   text: "",
   index: 0,
 };
 
-const base_heading = {
+const editor_base_heading = {
   type: "heading",
   text: "",
   index: 0,
 };
 
-const base_image = {
+const editor_base_image = {
   type: "image",
   alt_text: "",
   index: 0,
@@ -21,51 +21,54 @@ const base_image = {
 };
 
 // template refferences
-const paragraph_template = document.getElementById(
+const editor_paragraph_template = document.getElementById(
   "article-paragraph-template"
 );
-const image_template = document.getElementById("article-image-template");
-const heading_template = document.getElementById("article-heading-template");
+const editor_image_template = document.getElementById("article-image-template");
+const editor_heading_template = document.getElementById(
+  "article-heading-template"
+);
+
 // render refference
-const container = document.getElementById("editor-container");
+const editor_container = document.getElementById("editor-container");
 
 // view functions
-function generate_preview() {
-  container.innerHTML = null;
+function editor_generate_preview() {
+  editor_container.innerHTML = null;
   let entry;
   let item;
 
-  for (let index = 0; index < article.length; index++) {
-    switch (article[index].type) {
+  for (let index = 0; index < editor_article.length; index++) {
+    switch (editor_article[index].type) {
       case "paragraph":
         // append the paragraph template to the DOM
         entry = document.createElement("li");
-        item = paragraph_template.cloneNode(true).content;
+        item = editor_paragraph_template.cloneNode(true).content;
         entry.appendChild(item);
-        container.appendChild(entry);
+        editor_container.appendChild(entry);
 
         // connect the input to the article item
         let textarea = entry.querySelector(".paragraph-input");
-        textarea.value = article[index].text;
+        textarea.value = editor_article[index].text;
         textarea.onchange = () => {
-          article[index].text = textarea.value;
+          editor_article[index].text = textarea.value;
         };
         break;
 
       case "image":
         // append the image template to the DOM
         entry = document.createElement("li");
-        item = image_template.cloneNode(true).content;
+        item = editor_image_template.cloneNode(true).content;
         entry.appendChild(item);
-        container.appendChild(entry);
+        editor_container.appendChild(entry);
 
         // connect the image-template to the relevant item
         let display = entry.querySelector(".image-display");
-        display.src = article[index].src;
+        display.src = editor_article[index].src;
         let alt_text = entry.querySelector(".image-alt-text-input");
-        alt_text.value = article[index].alt_text;
+        alt_text.value = editor_article[index].alt_text;
         alt_text.onchange = () => {
-          article[index].alt_text = alt_text.value;
+          editor_article[index].alt_text = alt_text.value;
         };
 
         break;
@@ -76,94 +79,94 @@ function generate_preview() {
       // skip to next item if we dont have a template for the type
       default:
         console.log("skipping, unkown type");
-        console.log(article[index]);
+        console.log(editor_article[index]);
         continue;
     }
 
     // hook up the delete button
     let delete_button = entry.querySelector(".delete-button");
     delete_button.onclick = () => {
-      remove_item(index);
-      generate_preview();
+      editor_remove_item(index);
+      editor_generate_preview();
     };
 
     // hook up the buttons to move item up and down
     let move_up_button = entry.querySelector(".move-up-button");
     move_up_button.onclick = () => {
-      move_item_up(index);
-      generate_preview();
+      editor_move_item_up(index);
+      editor_generate_preview();
     };
 
     let move_down_button = entry.querySelector(".move-down-button");
     move_down_button.onclick = () => {
-      move_item_down(index);
-      generate_preview();
+      editor_move_item_down(index);
+      editor_generate_preview();
     };
   }
 }
 
 // article functions
-function add_item(item) {
-  let length = article.length;
+function editor_add_item(item) {
+  let length = editor_article.length;
   item.index = length;
-  article.push(item);
-  generate_preview();
+  editor_article.push(item);
+  editor_generate_preview();
 }
 
-function remove_item(index) {
-  article.splice(index, 1);
+function editor_remove_item(index) {
+  editor_article.splice(index, 1);
 }
 
-function move_item_down(index) {
-  let item = article[index];
-  article[index] = article[index + 1];
-  article[index + 1] = item;
+function editor_move_item_down(index) {
+  let item = editor_article[index];
+  editor_article[index] = editor_article[index + 1];
+  editor_article[index + 1] = item;
   // assign new indexes
-  article[index].index = index;
-  article[index + 1].index = index + 1;
+  editor_article[index].index = index;
+  editor_article[index + 1].index = index + 1;
 }
 
-function move_item_up(index) {
-  let item = article[index];
-  article[index] = article[index - 1];
-  article[index - 1] = item;
+function editor_move_item_up(index) {
+  let item = editor_article[index];
+  editor_article[index] = editor_article[index - 1];
+  editor_article[index - 1] = item;
   // assign new indexes
-  article[index].index = index;
-  article[index - 1].index = index - 1;
+  editor_article[index].index = index;
+  editor_article[index - 1].index = index - 1;
 }
 
-function add_paragraph() {
-  let p = { ...base_paragraph }; // clone the base paragraph;
-  add_item(p);
+function editor_add_paragraph() {
+  let p = { ...editor_base_paragraph }; // clone the base paragraph;
+  editor_add_item(p);
 }
 
-function add_image() {
-  let i = { ...base_image }; // clone the base paragraph;
-  add_item(i);
+function editor_add_image() {
+  let i = { ...editor_base_image }; // clone the base paragraph;
+  editor_add_item(i);
 }
 
-function add_heading() {
-  let h = { ...base_heading }; // clone the base paragraph;
-  add_item(h);
+function editor_add_heading() {
+  let h = { ...editor_base_heading }; // clone the base paragraph;
+  editor_add_item(h);
 }
 
-function log_article() {
-  console.log(article);
+function editor_log_article() {
+  console.log(editor_article);
 }
 
 // startup
-function gallery_init() {
+function editor_init() {
   let log_button = document.getElementById("log-article");
-  log_button.onclick = log_article;
+  log_button.onclick = editor_log_article;
 
   let add_paragraph_button = document.getElementById("add-paragraph");
-  add_paragraph_button.onclick = add_paragraph;
+  add_paragraph_button.onclick = editor_add_paragraph;
 
   let add_image_button = document.getElementById("add-image");
-  add_image_button.onclick = add_image;
+  add_image_button.onclick = editor_add_image;
 
   let add_heading_button = document.getElementById("add-heading");
-  add_heading_button.onclick = add_heading;
+  add_heading_button.onclick = editor_add_heading;
 }
 
-window.onload = gallery_init;
+editor_init();
