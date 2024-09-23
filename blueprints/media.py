feb4b66670408c8e79f67a75a570faf984f3f2e2
human <70441087+nyancel@users.noreply.github.com>
2024-09-23 17:32:07 +0200

    media args and returns

--- blueprints/media.py
+++ blueprints/media.py
@@ -12,7 +12,8 @@ bp = flask.Blueprint("media", __name__)
 def upload_media():
     if "media" not in flask.request.files:
         return (
-            flask.jsonify({"error": "post request is missing a file labeled 'media'"}),
+            flask.jsonify(
+                {"error": "post request is missing a file labeled 'media'"}),
             400,
         )
 
@@ -29,41 +30,43 @@ def upload_media():
 
 @bp.post("/media/fetch_media")
 def fetch_media():
-    media_ID = flask.request.args.get("media_ID")
-    
+    json: dict = flask.request.json
+    media_ID = json.get("media_ID")
+
     if not media_ID:
         return flask.jsonify(
             {"error": "no media_ID given"},
             400
         )
-    
+
     data = lib.media.fetch.get_media_full(media_ID)
-    
+
     if isinstance(data, Exception):
         return flask.jsonify({"error": data.args[0]}), 400
 
     return flask.jsonify(data), 200
 
+
 @bp.get("/media/fetch_all_media_metadata")
 def fetch_all_media_metadata():
     data = lib.media.fetch.get_all_media_metadata()
-    
+
     if not data:
         return flask.jsonify({"error": "no metadata could be found"}), 400
 
     return flask.jsonify(data), 200
 
-    
+
 @bp.get("/media/fetch_media_instance")
 def fetch_media_instance():
     instance_ID = flask.request.args.get("instance_ID")
-    
+
     if not instance_ID:
         return flask.jsonify(
             {"error": "no instance_ID given"},
             400
         )
-    
+
     data = lib.media.fetch.get_specific_media_instance(instance_ID)
 
     if not data:
@@ -71,19 +74,19 @@ def fetch_media_instance():
             {"error": "an unhandled error occured whilst fetching instance data"},
             400
         )
-    
+
     parent_ID = data["parent_id"]
     metadata = lib.media.fetch.get_media_metadata(parent_ID)
-    
+
     if not metadata:
         return flask.jsonify(
             {"error": "an unhandled error occured whilst fetching metadata"},
             400
         )
-    
+
     file_extention = metadata["file_extention"]
-    filename = metadata["filename"]
-    
+    filename = f"{metadata["filename"]}.{metadata["file_extention"]}"
+
     filepath = f"volume/media/files/{parent_ID}/{instance_ID}.{file_extention}"
-    
+
     return flask.send_file(filepath, download_name=filename)
