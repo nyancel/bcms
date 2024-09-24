9633dc75c08259e6a76e923bf9f643b3481ca923
human <70441087+nyancel@users.noreply.github.com>
2024-09-24 11:35:38 +0200

    DB tabel for privs

--- lib/user/user_db.py
+++ lib/user/user_db.py
@@ -83,3 +83,54 @@ class UserToken(Driver.BASE):
         _dict["created_at"] = self.created_at
         _dict["expires_at"] = self.expires_at
         return _dict
+
+
+class UserRights(Driver.BASE):
+    __tablename__ = "user_token"
+    id: orm.Mapped[str] = orm.mapped_column(primary_key=True)
+    user_id: orm.Mapped[str]
+    # article privs
+    can_post_draft: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_approve_draft: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_publish_article: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_delete_article: orm.Mapped[bool] = orm.mapped_column(default=False)
+    # media privs
+    can_post_media: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_unlist_media: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_delete_media: orm.Mapped[bool] = orm.mapped_column(default=False)
+    # user privs
+    can_assign_journalist: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_assign_editorial: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_assign_admin: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_delete_other_user: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_change_other_email: orm.Mapped[bool] = orm.mapped_column(default=False)
+    can_change_other_password: orm.Mapped[bool] = orm.mapped_column(
+        default=False)
+    can_change_other_details: orm.Mapped[bool] = orm.mapped_column(
+        default=False)
+    # event details
+    can_submit_event: orm.Mapped[bool] = orm.mapped_column(default=False)
+
+    def __init__(self, **kw: sql.Any):
+        super().__init__(**kw)
+        self.id = crypt.new_uid()
+        return self
+
+    def to_dict(self):
+        _dict = {}
+        _dict["can_post_draft"] = self.can_post_draft
+        _dict["can_approve_draft"] = self.can_approve_draft
+        _dict["can_publish_article"] = self.can_publish_article
+        _dict["can_delete_article"] = self.can_delete_article
+        _dict["can_post_media"] = self.can_post_media
+        _dict["can_unlist_media"] = self.can_unlist_media
+        _dict["can_delete_media"] = self.can_delete_media
+        _dict["can_assign_journalist"] = self.can_assign_journalist
+        _dict["can_assign_editorial"] = self.can_assign_editorial
+        _dict["can_assign_admin"] = self.can_assign_admin
+        _dict["can_delete_other_user"] = self.can_delete_other_user
+        _dict["can_change_other_email"] = self.can_change_other_email
+        _dict["can_change_other_password"] = self.can_change_other_password
+        _dict["can_change_other_details"] = self.can_change_other_details
+        _dict["can_submit_event"] = self.can_submit_event
+        return _dict
