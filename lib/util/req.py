442e2de372c92e8e05b130330b086f32518578e2
human <70441087+nyancel@users.noreply.github.com>
2024-09-24 18:32:30 +0200

    util update

--- lib/util/req.py
+++ lib/util/req.py
@@ -5,12 +5,25 @@ import lib.util.env as env
 
 
 def fetch_user_from_token(token_id: str):
-    target = f"{env.SERVER_ADRESS}/user/me"
+    target = f"{env.SERVER_ADRESS}/user/who"
     headers = {
         "content-type": "application/json"
     }
     data = {
         "user_token": token_id
+    }
+    r = requests.post(target, headers=headers, json=data)
+    j: dict = json.loads(r.text)
+    return j
+
+
+def fetch_rights_from_user(user_id: str):
+    target = f"{env.SERVER_ADRESS}/user/rights"
+    headers = {
+        "content-type": "application/json"
+    }
+    data = {
+        "user_id": user_id
     }
     r = requests.post(target, headers=headers, json=data)
     j: dict = json.loads(r.text)
