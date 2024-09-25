8c97f3990c6915561ea19a0fe5f043d2bc94be9d
human <70441087+nyancel@users.noreply.github.com>
2024-09-25 13:03:28 +0200

    edit user rights methdos

--- lib/user/rights.py
+++ lib/user/rights.py
@@ -12,10 +12,18 @@ def create_new_user_rights(user_id) -> user_db.UserRights:
     return new_rights
 
 
+def get_rights(rights_id) -> user_db.UserRights:
+    with user_db.Driver.SessionMaker() as db_session:
+        query = db_session.query(user_db.UserRights)
+        query = query.where(user_db.UserRights.id == rights_id)
+        rights = query.first()
+    return rights
+
+
 def get_user_rights(user_id) -> user_db.UserRights:
     with user_db.Driver.SessionMaker() as db_session:
         query = db_session.query(user_db.UserRights)
-        query = query.where(user_id == user_id)
+        query = query.where(user_db.UserRights.user_id == user_id)
         rights = query.first()
     return rights
 
@@ -26,3 +34,15 @@ def save_user_rights(rights: user_db.UserRights) -> user_db.UserRights:
         db_session.add(rights)
         db_session.commit()
     return rights
+
+
+def update_rights(rights_id: str, args: dict):
+    property_names = [p for p in dir(user_db.UserRights) if isinstance(
+        getattr(user_db.UserRights, p), property
+    )]
+    property_names = [p for p in property_names if p.startswith("can_")]
+    rights = get_rights(rights_id)
+    for p in property_names:
+        if args.get(p) is not None:
+            setattr(rights, p, args.get(p))
+    save_user_rights(rights)
