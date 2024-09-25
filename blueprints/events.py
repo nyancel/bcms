68682c672de871fe7e2f751d7f6bcb98aac4b45e
agu078 <agu078@uit>
2024-09-25 13:17:19 +0200

    modified:   blueprints/events.py 	modified:   lib/events/event_db.py 	modified:   lib/events/events.py

--- blueprints/events.py
+++ blueprints/events.py
@@ -1,4 +1,7 @@
 import flask
+import lib.events.event_db as event_db
+
+import lib.events.events as events
 
 bp = flask.Blueprint("event_blueprint", __name__)
 bp.url_prefix = "/event/"
@@ -9,7 +12,9 @@ def subscribe():
     endpoint = json.get("endpoint")
     key = json.get("key")
 
-    subscribers.add(endpoint, key)
+    events.notify_subscribers(key,endpoint)
+    
+    # subscribers.add(endpoint, key)
 
 @bp.post("submit")
 def submit():
@@ -17,8 +22,19 @@ def submit():
     event_data = json.get("event_data")
     key = json.get("key")
 
-    for s in subscribers:
-        request.send(s.endpoint, payload=event_data)
+    if None in [event_data, key]:
+        return flask.jsonify(
+            {"error": "missing event data or key"}
+        ), 400
+
+    # Create a new event using the `submit_event` function
+    event_id = events.submit_event(key, event_data)
+    
+    # Notify all subscribers
+    # events.notify_subscribers(key, event_id)
+
+    return flask.jsonify({"success": True, "event_id": event_id}), 200
+
 
 @bp.post("lookup")
 def lookup():
