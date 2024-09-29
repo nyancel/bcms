2923687f0582bffe183c501d4ccaa6cd175372e2
human <70441087+nyancel@users.noreply.github.com>
2024-09-30 01:56:18 +0200

    some more TS work

--- ts/src/editor.ts
+++ ts/src/editor.ts
@@ -1,91 +1,148 @@
-const C_EDITOR_ARTICLE = {
+import { time, fetch_formdata, get_smallest_res_from_src } from "./util";
+
+// types and declerations
+declare global {
+    interface Window {
+        receive_data: (data: string) => void;
+    }
+}
+
+enum ItemTypeEnum {
+    paragraph,
+    image,
+    heading
+}
+
+type TextItem = {
+    type: ItemTypeEnum.heading | ItemTypeEnum.paragraph,
+    text: string,
+    index: number,
+}
+
+type MediaItem = {
+    type: ItemTypeEnum.image,
+    alt_text: string,
+    src_id: string | undefined,
+    index: number,
+}
+
+type Article = {
+    id: undefined | number,
+    author_id: undefined | number;
+    last_changed: undefined | number;
+    content: Array<TextItem | MediaItem> | undefined;
+}
+
+// global constants
+const ARTICLE: Article = {
     id: undefined,
     author_id: undefined,
     last_changed: undefined,
     content: [],
 };
 
-// default objects and const reffs
-const C_EDITOR_BASE_PARAGRAPH = {
-    type: "paragraph",
-    text: "",
-    index: 0,
-};
+const TEMPLATES = {
+    paragraph: document.getElementById("article-paragraph-template"),
+    image: document.getElementById("article-paragraph-template"),
+    heading: document.getElementById("article-heading-template"),
+}
 
-const C_EDITOR_BASE_HEADING = {
-    type: "heading",
-    text: "",
-    index: 0,
-};
+const EDITOR = document.getElementById("editor-container");
 
-const C_EDITOR_BASE_IMAGE = {
-    type: "image",
-    alt_text: "",
-    index: 0,
-    image_id: undefined,
-};
-
-const C_EDITOR_PARAGRAPH_TEMPLATE = document.getElementById(
-    "article-paragraph-template"
-);
-const C_EDITOR_IMAGE_TEMPLATE = document.getElementById(
-    "article-image-template"
-);
-const C_EDITOR_HEADING_TEMPLATE = document.getElementById(
-    "article-heading-template"
-);
-
-const C_EDITOR_CONTAINER = document.getElementById("editor-container");
 
 // editor image functions
-function editor_gallery_pop_up_select(index) {
+function editor_gallery_pop_up_select(index: number) {
     const popup = window.open(
         "/gallery-popup",
         "popupWindow",
         "width=600,height=400"
     );
 
-    window.receive_data = function (data) {
-        C_EDITOR_ARTICLE.content[index].image_id = data.image_id;
+    window.receive_data = function (data: string) {
+        if (!ARTICLE.content) {
+            return;
+        }
+
+        let item = ARTICLE.content[index];
+        if (item.type != ItemTypeEnum.image) {
+            return;
+        }
+        item.src_id = data;
         editor_generate_preview();
     };
 }
 
-function editor_image_upload(index) {
+function editor_image_upload(index: number) {
     // select a file
     let input = document.createElement("input");
     input.type = "file";
     input.accept = "png, jpg, jpeg";
     input.multiple = false;
     input.onchange = async () => {
+        if (!ARTICLE.content) {
+            return;
+        }
+        let media_item = ARTICLE.content[index];
+        if (media_item.type != ItemTypeEnum.image) {
+            return;
+        }
         // retrieve the selected file
-        let files = Array.from(input.files);
+        let input_files = input.files;
+        if (!input_files) {
+            return;
+        }
+        let files = Array.from(input_files);
         let file = files[0];
         input.remove();
 
         // upload the file to the gallery
         let formdata = new FormData();
         formdata.append("media", file);
-        let image = await util_fetch_post_formdata("/media/upload_media", formdata);
+        let image = await fetch_formdata("/media/upload_media", formdata);
         let image_id = image.data.results[0].key;
-        C_EDITOR_ARTICLE.content[index].image_id = image_id;
-        C_EDITOR_ARTICLE.last_changed = util_epoch_seconds();
+        // set the item source
+        media_item.src_id = image_id;
+        ARTICLE.last_changed = time();
         editor_generate_preview();
     };
     input.click();
 }
 
-function editor_image_render(entry, index) {
-    // if image_id is undefined we have not yet chosen an image.
-    let display = entry.querySelector(".image-display");
-    if (!C_EDITOR_ARTICLE.content[index].image_id) {
+function editor_image_render(entry: HTMLElement, index: number) {
+    let display = entry.querySelector(".image-display") as HTMLImageElement;
+    if (!display) {
+        throw new Error("media-display not found");
+    }
+
+    if (!ARTICLE.content) {
+        throw new Error("article content not initialized");
+    }
+
+    let media_item = ARTICLE.content[index];
+    if (media_item.type != ItemTypeEnum.image) {
+        throw new Error("item type is not image");
+    }
+
+    // if src_id is undefined we have not yet chosen an image.
+    if (!media_item.src_id) {
         display.classList.toggle("hidden", true);
         return;
     }
+
     // else image_id is defined and we render the image
     const load = async () => {
-        display.src = await util_get_media_src_by_width(
-            C_EDITOR_ARTICLE.content[index].image_id,
+        if (!ARTICLE.content) {
+            throw new Error("article content not initialized");
+        }
+        let media_item = ARTICLE.content[index];
+        if (media_item.type != ItemTypeEnum.image) {
+            throw new Error("item type is not image");
+        }
+        if (!media_item.src_id) {
+            throw new Error("src_id not found");
+        }
+        display.src = await get_smallest_res_from_src(
+            media_item.src_id,
             entry.clientWidth
         );
         display.classList.toggle("hidden", false);
@@ -93,19 +150,25 @@ function editor_image_render(entry, index) {
     load();
 }
 
-function editor_insert_template(template) {
+function editor_insert_template(template: HTMLTemplateElement) {
+    if (!EDITOR) {
+        throw new Error("Editor has not been initalized");
+    }
+
     let entry = document.createElement("li");
-    let item = template.cloneNode(true).content;
+    let clone = template.cloneNode(true) as HTMLTemplateElement;
+    let item = clone.content;
+
     entry.appendChild(item);
-    C_EDITOR_CONTAINER.appendChild(entry);
+    EDITOR.appendChild(entry);
     return entry;
 }
 
-function editor_connect_paragraph(entry, index) {
-    let textarea = entry.querySelector(".paragraph-input");
-    textarea.value = C_EDITOR_ARTICLE.content[index].text;
+function editor_connect_paragraph(entry: HTMLElement, index: number) {
+    let textarea = entry.querySelector(".paragraph-input") as HTMLTextAreaElements;
+    textarea.value = ARTICLE.content[index].text;
     textarea.onchange = () => {
-        C_EDITOR_ARTICLE.content[index].text = textarea.value;
+        ARTICLE.content[index].text = textarea.value;
     };
 }
 
@@ -131,7 +194,7 @@ function editor_connect_generic(entry, index) {
     // generic delete
     let delete_button = entry.querySelector(".delete-button");
     delete_button.onclick = () => {
-        editor_remove_item(index);
+        remove_item_from_article(index);
         editor_generate_preview();
     };
 
@@ -190,86 +253,103 @@ function editor_generate_preview() {
 }
 
 // article functions
-function editor_add_item(item) {
-    let length = C_EDITOR_ARTICLE.content.length;
-    item.index = length;
-    C_EDITOR_ARTICLE.content.push(item);
-    C_EDITOR_ARTICLE.last_changed = util_epoch_seconds();
+function editor_add_item(item_type: ItemTypeEnum) {
+
+    if (!ARTICLE.content) {
+        ARTICLE.content = [];
+    }
+
+    let length = ARTICLE.content.length;
+    let new_item: TextItem | MediaItem;
+
+    switch (item_type) {
+        case ItemTypeEnum.paragraph:
+        case ItemTypeEnum.heading:
+            new_item = {
+                type: item_type,
+                text: "",
+                index: length
+            }
+            break;
+        case ItemTypeEnum.image:
+            new_item = {
+                type: item_type,
+                alt_text: "",
+                index: length,
+                src_id: undefined,
+            }
+            break;
+    }
+
+    ARTICLE.content.push(new_item);
+    ARTICLE.last_changed = time();
     editor_generate_preview();
 }
 
-function editor_remove_item(index) {
-    C_EDITOR_ARTICLE.content.splice(index, 1);
-    C_EDITOR_ARTICLE.last_changed = util_epoch_seconds();
+function remove_item_from_article(index: number) {
+    ARTICLE.content?.splice(index, 1);
+    ARTICLE.last_changed = time();
 }
 
-function editor_move_item_down(index) {
-    if (index > C_EDITOR_ARTICLE.content.length - 2) {
+function editor_move_item_down(index_to_change: number) {
+    if (!ARTICLE.content) {
         return;
     }
-    let item = C_EDITOR_ARTICLE.content[index];
-    C_EDITOR_ARTICLE.content[index] = C_EDITOR_ARTICLE.content[index + 1];
-    C_EDITOR_ARTICLE.content[index + 1] = item;
-    // assign new indexes
-    C_EDITOR_ARTICLE.content[index].index = index;
-    C_EDITOR_ARTICLE.content[index + 1].index = index + 1;
 
-    C_EDITOR_ARTICLE.last_changed = util_epoch_seconds();
-}
-
-function editor_move_item_up(index) {
-    if (index < 1) {
+    if (index_to_change >= ARTICLE.content.length - 1) {
         return;
     }
-    let item = C_EDITOR_ARTICLE.content[index];
-    C_EDITOR_ARTICLE.content[index] = C_EDITOR_ARTICLE.content[index - 1];
-    C_EDITOR_ARTICLE.content[index - 1] = item;
+
+    let item = ARTICLE.content[index_to_change];
+    ARTICLE.content[index_to_change] = ARTICLE.content[index_to_change + 1];
+    ARTICLE.content[index_to_change + 1] = item;
     // assign new indexes
-    C_EDITOR_ARTICLE.content[index].index = index;
-    C_EDITOR_ARTICLE.content[index - 1].index = index - 1;
+    ARTICLE.content[index_to_change].index = index_to_change;
+    ARTICLE.content[index_to_change + 1].index = index_to_change + 1;
 
-    C_EDITOR_ARTICLE.last_changed = util_epoch_seconds();
+    ARTICLE.last_changed = time();
 }
 
-function editor_add_paragraph() {
-    let p = { ...C_EDITOR_BASE_PARAGRAPH }; // clone the base paragraph;
-    editor_add_item(p);
-}
+function editor_move_item_up(index_to_change: number) {
+    if (!ARTICLE.content) {
+        return;
+    }
 
-function editor_add_image() {
-    let i = { ...C_EDITOR_BASE_IMAGE }; // clone the base paragraph;
-    editor_add_item(i);
-}
+    if (index_to_change < 1) {
+        return;
+    }
+    let item = ARTICLE.content[index_to_change];
+    ARTICLE.content[index_to_change] = ARTICLE.content[index_to_change - 1];
+    ARTICLE.content[index_to_change - 1] = item;
+    // assign new indexes
+    ARTICLE.content[index_to_change].index = index_to_change;
+    ARTICLE.content[index_to_change - 1].index = index_to_change - 1;
 
-function editor_add_heading() {
-    let h = { ...C_EDITOR_BASE_HEADING }; // clone the base paragraph;
-    editor_add_item(h);
+    ARTICLE.last_changed = time();
 }
 
 function editor_log_article() {
-    console.log(C_EDITOR_ARTICLE.content);
-}
-
-// startup
-function editor_init() {
-
+    console.log(ARTICLE.content);
 }
 
 export default function main() {
     let log_button = document.getElementById("log-article");
     if (log_button) {
-        log_button.onclick = editor_log_article;
+        log_button.onclick = () => editor_log_article();
     }
 
     let add_paragraph_button = document.getElementById("add-paragraph");
     if (add_paragraph_button) {
-        add_paragraph_button.onclick = () => editor_add_item({ ...C_EDITOR_BASE_PARAGRAPH, });
+        add_paragraph_button.onclick = () => editor_add_item(ItemTypeEnum.paragraph);
     }
 
     let add_image_button = document.getElementById("add-image");
-    add_image_button.onclick = () => editor_add_item({ ...C_EDITOR_BASE_IMAGE });
+    if (add_image_button) {
+        add_image_button.onclick = () => editor_add_item(ItemTypeEnum.image);
+    }
 
     let add_heading_button = document.getElementById("add-heading");
-    add_heading_button.onclick = () => editor_add_item({ ...C_EDITOR_BASE_HEADING, });
-
+    if (add_heading_button) {
+        add_heading_button.onclick = () => editor_add_item(ItemTypeEnum.heading);
+    }
 }
---
+++ ts/src/util.ts
@@ -0,0 +1,136 @@
+// typing
+type CacheItem = {
+    key: string,
+    item: string,
+    expires_at: number
+}
+
+// globals
+let CACHE: Array<CacheItem> = []; // cache for functions
+const CACHE_KEY = "C_CACHE_KEY";
+
+// internal functions
+function save_cache_to_localstorage() {
+    localStorage.setItem(CACHE_KEY, JSON.stringify(CACHE));
+}
+
+function load_cache_from_localstorage() {
+    let local = localStorage.getItem(CACHE_KEY);
+    if (!local) {
+        return;
+    }
+    let tmp = JSON.parse(local);
+    if (!tmp) {
+        return;
+    }
+    CACHE = JSON.parse(tmp);
+}
+
+function cache_get(key: string) {
+    // load up the cache
+    load_cache_from_localstorage();
+
+    // remove stale keys
+    for (let index = 0; index < CACHE.length; index++) {
+        if (CACHE[index].expires_at < time()) {
+            CACHE.splice(index, 1);
+        }
+    }
+
+    // save any changes
+    save_cache_to_localstorage();
+
+    // find the item
+    for (let index = 0; index < CACHE.length; index++) {
+        if (CACHE[index].key == key) {
+            return CACHE[index].item;
+        }
+    }
+    return null;
+}
+
+function cache_add(key: string, value: string) {
+    load_cache_from_localstorage();
+    let c: CacheItem = {
+        key: key,
+        item: value,
+        expires_at: time() + 3600, // valid for one hour
+    };
+
+    CACHE.push(c);
+    console.log(c);
+    save_cache_to_localstorage();
+}
+
+// exposed functions
+export function time() {
+    let d = new Date();
+    let current_time = d.getTime() / 1000;
+    return current_time;
+}
+
+
+export async function fetch_json(endpoint: string, data: Object) {
+    let response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    let response_json = await response.json();
+    return response_json;
+}
+
+export async function fetch_formdata(endpoint: string, data: FormData) {
+    let response = await fetch(endpoint, {
+        method: "POST",
+        body: data,
+    });
+
+    let json_response = await response.json();
+    return json_response;
+}
+
+export async function get_smallest_res_from_src(image_id: string, min_width: number) {
+    // check the cache first
+    let key = `util_get_media_src_by_width${image_id}${min_width}`;
+    let cached_value = cache_get(key);
+    if (cached_value) {
+        console.log("cache hit");
+        return cached_value;
+    }
+
+    let image_metadata = await fetch_json("/media/fetch_media", {
+        media_ID: image_id,
+    });
+
+    let min = Math.pow(min_width, 2);
+
+    let resolutions = await Promise.all(
+        image_metadata.instances.map((res: { instance_id: string, x_dimension: number, y_dimension: number }) => {
+            return {
+                id: res.instance_id,
+                resolution: res.x_dimension * res.y_dimension,
+            };
+        })
+    );
+
+    // by default pick the highest res, then loop over and select the lowest
+    // res that is acceptable for the display.
+    resolutions = resolutions.sort((a, b) => a.resolution - b.resolution);
+    let instance_id = resolutions[resolutions.length - 1].id;
+    for (let r of resolutions) {
+        if (r.resolution > min) {
+            instance_id = r.id;
+            break; // break once the first image res satisfies our condition
+        }
+    }
+
+    // format target url
+    let target_url = `/media/fetch_media_instance?instance_ID=${instance_id}`;
+    // populate cache before returning
+    cache_add(key, target_url);
+    return target_url;
+}
