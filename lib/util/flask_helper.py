11e5425d7806402f181eb218db1b18849203f482
human <70441087+nyancel@users.noreply.github.com>
2024-10-09 14:29:29 +0200

    small rewrite

---
+++ lib/util/flask_helper.py
@@ -0,0 +1,17 @@
+import flask
+import time
+
+
+def generate_response(data, code: int = 200, message: str = None):
+    if message or code != 200:
+        return flask.jsonify({
+            "time": time.time(),
+            "message": message or "Server failed",
+            "data": data or {},
+        }), code
+
+    return flask.jsonify({
+        "time": time.time(),
+        "message": "OK",
+        "data": data or {},
+    }), code
---
+++ lib/util/user_api.py
@@ -0,0 +1,22 @@
+import lib.util.env as env
+import lib.user.user as user
+import lib.user.token as token
+import lib.user.rights as rights
+
+
+def fetch_user_from_token(token_id: str):
+    t = token.get_token(token_id)
+    u = user.get_user(t.id)
+    return u
+
+
+def fetch_rights_from_user(user_id: str):
+    u = user.get_user(user_id)
+    r = rights.get_user_rights(u.id)
+    return r
+
+
+def get_admin_token() -> tuple[dict, int]:
+    u = user.get_user_by_email(env.SERVER_ADMIN_EMAIL)
+    t = token.create_new_token(u.id)
+    return t
