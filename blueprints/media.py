f7826abf433f2e42aeaa658113a6c7e7c57636e0
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-10-11 12:53:52 +0200

    fix media blueprint to use shared util functions instead of internal ones

--- blueprints/media.py
+++ blueprints/media.py
@@ -1,13 +1,13 @@
 import threading
 import flask
 
-import lib.util.env
-import lib.util.req
-
 import lib.media.upload
 import lib.media.fetch
 import lib.media.morph
-import lib.media.lib
+
+import lib.util.env
+import lib.util.user_api
+import lib.util.flask_helper
 
 bp = flask.Blueprint("media", __name__)
 
@@ -15,41 +15,37 @@ bp = flask.Blueprint("media", __name__)
 @bp.post("/media/upload_media")
 def upload_media():
     auth_token = flask.request.form.get("auth_token")
-    rights = lib.media.lib.get_user_rights_from_auth_token(auth_token)
-    if not isinstance(rights, lib.util.req.UserRights):
-        return rights
+    user, rights = lib.util.user_api.get_user_and_rights_from_auth_token(auth_token)
     
     if rights.can_post_media != True:
-        return lib.media.lib.generic_error(f"you lack the permissions for this action", statuscode=401)
+        return lib.util.flask_helper.generate_response(message=f"you lack the permissions for this action", code=401)
 
     if "media" not in flask.request.files:
-        return lib.media.lib.generic_error("post request is missing a file labeled 'media'")
+        return lib.util.flask_helper.generate_response(message="post request is missing a file labeled 'media'", code=400)
 
     files = flask.request.files.getlist("media")
-    success_check = lib.media.upload.save_files(files)
+    data = lib.media.upload.save_files(files)
 
-    return flask.jsonify({
-            "success": 1,
-            "message": "File upload request recieved",
-            "data": success_check
-        }), 200
+    if data:
+        return lib.util.flask_helper.generate_response(data)
+    else:
+        return lib.util.flask_helper.generate_response(message="failed to save any files", code=500)
+
 
 @bp.post("/media/update_media_metadata")
 def update_media_metadata():
     json_body_data: dict = flask.request.json
     
     auth_token = json_body_data.get("auth_token")
-    rights = lib.media.lib.get_user_rights_from_auth_token(auth_token)
-    if not isinstance(rights, lib.util.req.UserRights):
-        return rights
+    user, rights = lib.util.user_api.get_user_and_rights_from_auth_token(auth_token)
     
     if rights.can_update_media != True:
-        return lib.media.lib.generic_error(f"you lack the permissions for this action", statuscode=401)
+        return lib.util.flask_helper.generate_response(message=f"you lack the permissions for this action", code=401)
     
     media_ID = json_body_data.get("media_ID")
 
     if not media_ID:
-        return lib.media.lib.generic_error("no media_ID value was supplied")
+        return lib.util.flask_helper.generate_response(message="no media_ID value was supplied", code=400)
     
     # ensure only certain values can be changed with this endpoint
     valid_metadata_morph_keys = [
@@ -67,46 +63,35 @@ def update_media_metadata():
     data = lib.media.morph.update_media_metadata(media_ID, new_metadata)
 
     if isinstance(data, dict):
-        return flask.jsonify({
-            "success": 1,
-            "new_metadata": data
-        }), 200
+        return lib.util.flask_helper.generate_response(data)
 
     if isinstance(data, Exception):
-        return lib.media.lib.generic_error(
-            "an exception occured",
-            {"error": data.args[0]}
-        )
+        return lib.util.flask_helper.generate_response(message=f"an exception occured, {data.args[0]}", code=400)
     
-    return flask.jsonify({"error": "an unhandled exception occured"}), 400
+    return lib.util.flask_helper.generate_response(message="an unhandled exception occured", code=400)
 
 @bp.post("/media/fetch_media")
 def fetch_media():
     json_body_data: dict = flask.request.json
     
     auth_token = json_body_data.get("auth_token")
-    rights = lib.media.lib.get_user_rights_from_auth_token(auth_token)
-    if not isinstance(rights, lib.util.req.UserRights):
-        return rights
+    user, rights = lib.util.user_api.get_user_and_rights_from_auth_token(auth_token)
     
     if rights.can_update_media != True:
-        return lib.media.lib.generic_error(f"you lack the permissions for this action", statuscode=401)
+        return lib.util.flask_helper.generate_response(message=f"you lack the permissions for this action", code=401)
     
     media_ID = json_body_data.get("media_ID")
 
     if not media_ID:
-        return lib.media.lib.generic_error("no media_ID given")
+        return lib.util.flask_helper.generate_response(message="no media_ID given", code=400)
 
     data = lib.media.fetch.get_media_full(media_ID)
     
 
     if isinstance(data, Exception):
-        return lib.media.lib.generic_error(
-            "an exception occured",
-            {"error": data.args[0]}
-        )
+        return lib.util.flask_helper.generate_response(message=f"an exception occured, {data.args[0]}", code=500)
     
-    return flask.jsonify(data), 200
+    return lib.util.flask_helper.generate_response(data)
 
 
 @bp.post("/media/fetch_all_media_metadata")
@@ -114,22 +99,20 @@ def fetch_all_media_metadata():
     json_body_data: dict = flask.request.json
     
     auth_token = json_body_data.get("auth_token")
-    rights = lib.media.lib.get_user_rights_from_auth_token(auth_token)
-    if not isinstance(rights, lib.util.req.UserRights):
-        return rights
+    user, rights = lib.util.user_api.get_user_and_rights_from_auth_token(auth_token)
     
     if rights.can_update_media != True:
-        return lib.media.lib.generic_error(f"you lack the permissions for this action", statuscode=401)
-    
+        return lib.util.flask_helper.generate_response(message=f"you lack the permissions for this action", code=401)
+
     data = lib.media.fetch.get_all_media_metadata()
 
     if data == []:
-        return lib.media.lib.generic_error("the server does not have any public media on it")
+        return lib.util.flask_helper.generate_response(message="the server does not have any public media on it", code=400)
 
     if not data:
-        return lib.media.lib.generic_error("no metadata could be found")
+        return lib.util.flask_helper.generate_response(message="no metadata could be found", code=400)
 
-    return flask.jsonify(data), 200
+    return lib.util.flask_helper.generate_response(data)
 
 
 @bp.get("/media/fetch_media_instance")
@@ -137,18 +120,18 @@ def fetch_media_instance():
     instance_ID = flask.request.args.get("instance_ID")
 
     if not instance_ID:
-        return lib.media.lib.generic_error("no instance_ID was specified")
+        return lib.util.flask_helper.generate_response(message="no instance_ID was specified", code=400)
 
     data = lib.media.fetch.get_specific_media_instance(instance_ID)
 
     if not data:
-        return lib.media.lib.generic_error("an unhandled error occured whilst fetching instance data")
+        return lib.util.flask_helper.generate_response(message="an unhandled error occured whilst fetching instance data", code=500)
 
     parent_ID = data["parent_id"]
     metadata = lib.media.fetch.get_media_metadata(parent_ID)
 
     if not metadata:
-        return lib.media.lib.generic_error("an unhandled error occured whilst fetching metadata")
+        return lib.util.flask_helper.generate_response(message="an unhandled error occured whilst fetching metadata", code=500)
 
     file_extention = metadata["file_extention"]
     filename = metadata["filename"]
--- lib/media/lib.py
+++ lib/media/lib.py
@@ -1,5 +1,4 @@
 import flask
-import lib.util.req
 
 def generic_error(error_message: str, statuscode = 400, **kwargs: dict[str: str]) -> flask.Response:
     response_payload = {
@@ -9,17 +8,3 @@ def generic_error(error_message: str, statuscode = 400, **kwargs: dict[str: str]
     response_payload.update(kwargs)
     
     return flask.jsonify(response_payload), statuscode
-
-def get_user_rights_from_auth_token(auth_token: str) -> lib.util.req.UserRights | flask.Response:
-    if not auth_token:
-        return generic_error("auth_token not found")
-
-    user, status_code = lib.util.req.fetch_user_from_token(auth_token)
-    if status_code != 200:
-        return generic_error("user data not found")
-
-    rights, status_code = lib.util.req.fetch_rights_from_user(user.get("id"))
-    if status_code != 200:
-        return generic_error("could not fetch user rights")
-
-    return rights
