b9b804b9e6689bd3df5b3112f295f15dcb797ed2
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-10-16 12:25:46 +0200

    i'm sick of forgetting to setup the env

--- init.py
+++ init.py
@@ -4,7 +4,13 @@ import scripts.init_user_db
 import scripts.init_folders
 import scripts.init_event_db
 
+import lib.util.env
+
 if __name__ == "__main__":
+    if not lib.util.env.USER_DB_PATH:
+        print("could not find `USER_DB_PATH` in your environment, perhaps you forgot your .env file?")
+        exit()
+    
     scripts.init_folders.run()
     scripts.init_user_db.run()  # USER is the first DB, to init test users that others depend on.
     scripts.init_media_db.run()
