0e96c88c80a2f19e46da63cca18cf3b5d6478111
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-10-19 01:56:43 +0200

    automatically generate a .env file if there isn't already one

--- init.py
+++ init.py
@@ -1,3 +1,11 @@
+import os
+
+# generate the .env for development purposes
+if __name__ == "__main__":
+    if not os.path.isfile(".env"):
+        import shutil
+        shutil.copy("docs/exampleenv.txt", ".env")
+
 import scripts.init_article_db
 import scripts.init_media_db
 import scripts.init_user_db
@@ -7,12 +15,13 @@ import scripts.init_event_db
 import lib.util.env
 
 if __name__ == "__main__":
-    if not lib.util.env.USER_DB_PATH:
-        print("could not find `USER_DB_PATH` in your environment, perhaps you forgot your .env file?")
-        exit()
-    
     scripts.init_folders.run()
     scripts.init_user_db.run()  # USER is the first DB, to init test users that others depend on.
     scripts.init_media_db.run()
     scripts.init_article_db.run()
     scripts.init_event_db.run()
+    
+    if os.path.isfile("None"):
+        print("-" * 60)
+        print("seems like you have a file named `None`, this might be due to a missing environment variable?")
+        print("-" * 60)
